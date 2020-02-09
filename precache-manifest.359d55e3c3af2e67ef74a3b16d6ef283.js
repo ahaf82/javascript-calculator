ced6ac6e54e1136bc1a492057dbb44de26eb3ce0
Armin Hafner <a_h82@gmx.net>
2020-02-09 17:42:38 +0100

    Updates

---
+++ precache-manifest.359d55e3c3af2e67ef74a3b16d6ef283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8fbf3aa4397785c86f7e9fcce532bc",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "b11576d452ae5e9c1e3d",
+    "url": "/javascript-calculator/static/css/main.805204da.chunk.css"
+  },
+  {
+    "revision": "3e9916e23d51ebc46f84",
+    "url": "/javascript-calculator/static/js/2.a43f6892.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/javascript-calculator/static/js/2.a43f6892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11576d452ae5e9c1e3d",
+    "url": "/javascript-calculator/static/js/main.620b4998.chunk.js"
+  },
+  {
+    "revision": "054eac1b9e1be10a5c81",
+    "url": "/javascript-calculator/static/js/runtime-main.aafb8b0a.js"
+  }
+]);
